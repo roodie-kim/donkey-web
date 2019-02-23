7bcefc6dcee9469d6117a58952645f26c7b1e7f3
hkahn <rood_optical@naver.com>
2019-02-23 23:17:36 +0900

    pm2 ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-    apps : [{
-        name      : 'sonnol', // App name that shows in `pm2 ls`
-        exec_mode : 'cluster', // enables clustering
-        instances : '4', // or an integer
-        cwd       : './', // only if using a subdirectory
-        script    : './node_modules/nuxt/bin/nuxt-start', // The magic key
-    }]
-};
+    apps: [
+        {
+            name: 'index-web',
+            // watch: 파일이 변경되면 서버가 자동으로 재시작
+            watch: false,
+            exec_mode: 'cluster',
+            // instances: <0: CPU 코어 갯수>
+            instances: 0,
+            // merge_logs: 실행 모드가 cluster 일 때 로그를 한 파일에 저장
+            merge_logs: true,
+            env: {
+                'NODE_ENV': 'production'
+            }
+        }
+    ]
+}
