d606f963b9bbb6c2c0ccbbfd548b3f49f21eed88
hkahn <rood_optical@naver.com>
2019-02-23 23:04:54 +0900

    pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps : [{
+        name      : 'sonnol', // App name that shows in `pm2 ls`
+        exec_mode : 'cluster', // enables clustering
+        instances : '4', // or an integer
+        cwd       : './current', // only if using a subdirectory
+        script    : './node_modules/nuxt/bin/nuxt-start', // The magic key
+    }]
+};
