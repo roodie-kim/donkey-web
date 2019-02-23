b398d903e96285c7c48429a1bffe85078208d090
hkahn <rood_optical@naver.com>
2019-02-24 03:28:45 +0900

    429 에러페이지 수정

--- components/errors/429.vue
+++ components/errors/429.vue
@@ -1,14 +1,13 @@
 <template>
     <div>
-        로딩중입니다!
+        많은 접속 시도가 있네요. 운이 좋으셨습니다.
+        <nuxt-link to="/">홈으로 가기</nuxt-link>
     </div>
 </template>
 
 <script>
 export default {
-    mounted() {
-        this.$router.push('/')
-    }
+
 }
 </script>
 
