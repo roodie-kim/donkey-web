a59341665af7a55e51cde20f82e46a76ebe97b09
hkahn <rood_optical@naver.com>
2019-02-24 11:42:45 +0900

    error message 수정

--- components/errors/500.vue
+++ components/errors/500.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        뭔가 서버에 문제가 생긴거 같아요.
-        <nuxt-link to="/">메인으로 돌아가기</nuxt-link>
+    <div class="error-page">
+        <div class="message-div">
+            <h3 class="is-title is-size-3 sentence">뭔가 서버에 문제가 생긴거 같아요.</h3>
+            <nuxt-link
+                class="button is-primary go-home-button"
+                to="/">메인으로 돌아가기</nuxt-link>
+        </div>
     </div>
 </template>
 
@@ -12,5 +16,19 @@ export default {
 </script>
 
 <style scoped>
-
+.error-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 200px;
+}
+.message-div {
+    text-align: center;
+}
+.sentence {
+    margin-bottom: 30px;
+}
+.go-home-button {
+    height: 50px;
+}
 </style>
--- pages/errors/toomanyrequests.vue
+++ pages/errors/toomanyrequests.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        로딩중입니다.
+    <div class="error-page">
+        <div class="message-div">
+            <h3 class="is-title is-size-3">사람이 많네요.</h3>
+            <h3 class="is-title is-size-3">잠시만 기다려주세요.</h3>
+            <h5 class="is-subtitle is-size-5">로딩중입니다.</h5>
+        </div>
     </div>
 </template>
 
@@ -13,5 +17,13 @@ export default {
 </script>
 
 <style scoped>
-
+.error-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 200px;
+}
+.message-div {
+    text-align: center;
+}
 </style>
