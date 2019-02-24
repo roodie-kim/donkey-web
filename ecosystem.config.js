5542e2eb6d303ae8f1fbe72c6c8cb2cb9482e304
hkahn <rood_optical@naver.com>
2019-02-24 11:45:45 +0900

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps : [{
         name      : 'sonnol', // App name that shows in `pm2 ls`
         exec_mode : 'cluster', // enables clustering
-        instances : max, // or an integer
+        instances : 'max', // or an integer
         script    : './node_modules/nuxt/bin/nuxt.js', // The magic key
     }]
 };
