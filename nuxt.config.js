53e5f923bc5bb0487483c1ad4fed13066f4206eb
hkahn <rood_optical@naver.com>
2019-02-24 22:13:30 +0900

    google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,7 @@ module.exports = {
         '@nuxtjs/axios',
         '@nuxtjs/dotenv',
         ['@nuxtjs/google-analytics', {
-            id: process.env.GA_ID || ''
+            id: GA_ID=UA-135088360-1
         }],
         'nuxt-device-detect',
         ['nuxt-buefy', { css: false, materialDesignIcons: false }],
